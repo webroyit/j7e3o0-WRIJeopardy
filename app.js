f70e436cf330fbe6c52ec959d8cb78ddc932ef88
royweb <xytank@gmail.com>
2022-08-02 11:44:14 -0400

    Add correct data for Jeopardy

--- app.js
+++ app.js
@@ -7,19 +7,19 @@ const jeopardyCategories = [
         questions: [
             {
                 question: "Who are you?",
-                answers: ["Water, Fire"],
+                answers: ["Water", "Fire"],
                 correct: "Water",
                 level: "easy"
             },
             {
                 question: "Who are you?",
-                answers: ["Water, Fire"],
+                answers: ["Water", "Fire"],
                 correct: "Water",
                 level: "medium"
             },
             {
                 question: "Who are you?",
-                answers: ["Water, Fire"],
+                answers: ["Water", "Fire"],
                 correct: "Water",
                 level: "hard"
             }
@@ -30,19 +30,19 @@ const jeopardyCategories = [
         questions: [
             {
                 question: "Where are you?",
-                answers: ["Home, Work"],
+                answers: ["Home", "Work"],
                 correct: "Home",
                 level: "easy"
             },
             {
                 question: "Where are you?",
-                answers: ["Home, Work"],
+                answers: ["Home", "Work"],
                 correct: "Home",
                 level: "medium"
             },
             {
                 question: "Where are you?",
-                answers: ["Home, Work"],
+                answers: ["Home", "Work"],
                 correct: "Home",
                 level: "hard"
             }
@@ -53,19 +53,19 @@ const jeopardyCategories = [
         questions: [
             {
                 question: "When did it snow?",
-                answers: ["Today, Last Week"],
+                answers: ["Today", "Last Week"],
                 correct: "Today",
                 level: "easy"
             },
             {
                 question: "When did it snow?",
-                answers: ["Today, Last Week"],
+                answers: ["Today", "Last Week"],
                 correct: "Today",
                 level: "medium"
             },
             {
                 question: "When did it snow?",
-                answers: ["Today, Last Week"],
+                answers: ["Today", "Last Week"],
                 correct: "Today",
                 level: "hard"
             }
@@ -76,19 +76,19 @@ const jeopardyCategories = [
         questions: [
             {
                 question: "What do bird eat?",
-                answers: ["Nothing, Worm"],
+                answers: ["Nothing", "Worm"],
                 correct: "Worm",
                 level: "easy"
             },
             {
                 question: "What do bird eat?",
-                answers: ["Nothing, Worm"],
+                answers: ["Nothing", "Worm"],
                 correct: "Worm",
                 level: "medium"
             },
             {
                 question: "What do bird eat?",
-                answers: ["Nothing, Worm"],
+                answers: ["Nothing", "Worm"],
                 correct: "Worm",
                 level: "hard"
             }
@@ -99,19 +99,19 @@ const jeopardyCategories = [
         questions: [
             {
                 question: "How many seconds in an hour?",
-                answers: ["36000, 3600"],
+                answers: ["36000", "3600"],
                 correct: "3600",
                 level: "easy"
             },
             {
                 question: "How many seconds in an hour?",
-                answers: ["36000, 3600"],
+                answers: ["36000", "3600"],
                 correct: "3600",
                 level: "medium"
             },
             {
                 question: "How many seconds in an hour?",
-                answers: ["36000, 3600"],
+                answers: ["36000", "3600"],
                 correct: "3600",
                 level: "hard"
             }
